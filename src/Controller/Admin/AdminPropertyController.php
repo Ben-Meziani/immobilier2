f5ef7a8b300e74fb384316fbbd715e6879f81656
ben <toufik-m@live.fr>
2020-05-28 02:23:08 +0200

    bug sur les roles

--- src/Controller/Admin/AdminPropertyController.php
+++ src/Controller/Admin/AdminPropertyController.php
@@ -3,8 +3,11 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Property;
+use App\Entity\User;
+use App\Form\EditUserType;
 use App\Form\PropertyType;
 use App\Repository\PropertyRepository;
+use App\Repository\UserRepository;
 use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,101 +15,133 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class AdminPropertyController extends AbstractController
 {
-     
-/**
- * @var PropertyRepository
- */
-private $repository;
 
-/**
- * @var EntityManagerInterface
- */
-private $em;
+    /**
+     * @var PropertyRepository
+     */
+    private $repository;
 
-public function __construct(PropertyRepository $repository, EntityManagerInterface $em)
-{
-    $this->repository = $repository;
-    $this->em = $em;
-}
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
 
-/** 
- * @Route("/admin", name="admin.property.index")
- * @return \Symfony\Component\HttpFoundation\Response
- */
-public function index()
-{
-   $properties = $this->repository->findAll();
-   return $this->render('admin/property/index.html.twig', compact('properties'));
-}
+    public function __construct(PropertyRepository $repository, EntityManagerInterface $em)
+    {
+        $this->repository = $repository;
+        $this->em = $em;
+    }
 
-/** 
- * @Route("/admin/property/create", name="admin.property.new")
- */
-public function new(Request $request) 
-{
-    $property = new Property();
-    $form = $this->createForm(PropertyType::class, $property);
-    $form->handleRequest($request);
-    $property->setCreatedAt(new DateTime());
-    
-    if ($form->isSubmitted() && $form->isValid())  {
-        $this->em->persist($property);
-        $this->em->flush();
-        $this->addFlash('success', 'Bien créé avec succès');
+    /** 
+     * @Route("/admin", name="admin.property.index")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function index()
+    {
+        $properties = $this->repository->findAll();
+        return $this->render('admin/property/index.html.twig', compact('properties'));
+    }
+
+    /** 
+     * @Route("/admin/property/create", name="admin.property.new")
+     */
+    public function new(Request $request)
+    {
+        $property = new Property();
+        $form = $this->createForm(PropertyType::class, $property);
+        $form->handleRequest($request);
+        $property->setCreatedAt(new DateTime());
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->persist($property);
+            $this->em->flush();
+            $this->addFlash('success', 'Bien créé avec succès');
+            return $this->redirectToRoute('admin.property.index');
+        }
+
+
+        return $this->render('admin/property/new.html.twig', [
+
+            "property" => $property,
+            "form" => $form->createView()
+        ]);
+    }
+
+    /** 
+     * @Route("/admin/property/{id}", name="admin.property.edit", methods="GET|POST")
+     * @param Property $property
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function edit(Property $property, Request $request)
+    {
+        $form = $this->createForm(PropertyType::class, $property);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->em->flush();
+            $this->addFlash('success', 'Bien modifié avec succès');
+            return $this->redirectToRoute('admin.property.index');
+        }
+
+
+        return $this->render('admin/property/edit.html.twig', [
+
+            "property" => $property,
+            "form" => $form->createView()
+        ]);
+    }
+    /**
+     *@Route("/admin/property/{id}", name="admin.property.delete", methods="DELETE")
+     * @param Property $property
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function delete(Property $property, Request $request)
+    {
+        if ($this->isCsrfTokenValid('delete' . $property->getId(), $request->get('_token'))) {
+            $this->em->remove($property);
+            $this->em->flush();
+            $this->addFlash('success', 'Bien supprimé avec succès');
+        }
         return $this->redirectToRoute('admin.property.index');
     }
-    
 
-    return $this->render('admin/property/new.html.twig', [
-        
-        "property" => $property, 
-        "form" => $form->createView()
-    ]);
-
-
-   
-}
-
-/** 
- * @Route("/admin/property/{id}", name="admin.property.edit", methods="GET|POST")
- * @param Property $property
- * @param Request $request
- * @return \Symfony\Component\HttpFoundation\Response
- */
-public function edit(Property $property, Request $request)
-{   
-    $form = $this->createForm(PropertyType::class, $property);
-    $form->handleRequest($request);
-
-    if ($form->isSubmitted() && $form->isValid()) {
-        $this->em->flush();
-        $this->addFlash('success', 'Bien modifié avec succès');
-        return $this->redirectToRoute('admin.property.index');
+    /**
+     * Undocumented function
+     *
+     * @Route("/admin/user", name="admin_user")
+     */
+    public function usersList(UserRepository $user)
+    {
+        return $this->render("admin/property/user.html.twig", [
+            'user' => $user->findAll()
+        ]);
     }
+    /**
+     * Modifier la route
+     *
+     * @Route("/admin/user/edituser/{id}", name="edit_user")
+     */
+    public function editUser(User $user, Request $request)
+    {
+        $form = $this->createForm(EditUserType::class, $user);
+        $form->handleRequest($request);
 
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
 
-    return $this->render('admin/property/edit.html.twig', [
-        
-        "property" => $property, 
-        "form" => $form->createView()
-    ]);
-}
-/**
- *@Route("/admin/property/{id}", name="admin.property.delete", methods="DELETE")
- * @param Property $property
- * @return \Symfony\Component\HttpFoundation\RedirectResponse
- */
-public function delete(Property $property,Request $request)
-{
-    if ($this->isCsrfTokenValid('delete'. $property->getId(), $request->get('_token'))) {
-         $this->em->remove($property);
-         $this->em->flush();
-         $this->addFlash('success', 'Bien supprimé avec succès');
+            $this->addFlash('message', 'Utilisateur modifié avec succès');
+            return $this->redirectToRoute('admin_user');
+
+        }
+        return $this->render('admin/property/edituser.html.twig', [
+            'userForm' => $form->createView()
+        ]);
     }
-   return $this->redirectToRoute('admin.property.index');
-}
-
-
 }
---
+++ src/Form/EditUserType.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Doctrine\DBAL\SQLParserUtilsException;
+use Doctrine\DBAL\Types\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class EditUserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username', TextType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Merci de bien remplir votre nom'
+                    ])
+                    ],
+                    'required' => true,
+                    'attr' => [
+                        'class' => 'form-control'
+                    ]
+            ])
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'Utilisateur' => 'ROLE_USER',
+                    'Editeur' => 'ROLE_EDITOR',
+                    'Modérateur' => 'ROLE_MODO',
+                    'Administrateur' => 'ROLE_ADMIN'
+                ],
+                'expanded' => true,
+                'multiple' => true,
+                'labele' => 'Rôles'
+            ])
+            ->add('Valider', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
