7cd6b0f804ad62d1bc33a51f849747c851157551
Ben-web-developement <toufik-m@live.fr>
2022-01-09 19:12:17 +0100

    Installation d'easyadmin

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
